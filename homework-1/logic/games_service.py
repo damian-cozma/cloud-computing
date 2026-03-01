1ee060d05c8b5a8433d2ea791d11ed7afa6abe95
Damian Cozma <damiancozma620@gmail.com>
2026-03-01 20:27:47 +0200

    implement data access layer for games

---
+++ homework-1/logic/games_service.py
@@ -0,0 +1,17 @@
+import json
+
+GAMES_FILE = "storage/games.json"
+
+def read_games():
+    try:
+        with open(GAMES_FILE, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        raise FileNotFoundError(f"{GAMES_FILE} not found")
+
+def read_game_by_id(game_id):
+    games = read_games()
+    for game in games:
+        if game["id"] == game_id:
+            return game
+    return None
