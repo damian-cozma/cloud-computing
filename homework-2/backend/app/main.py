2544455063386bf18b3fb19e92a2c53a93bb545f
Damian Cozma <damiancozma620@gmail.com>
2026-03-17 02:26:27 +0200

    initialize NewsAPI integration and global gaming news endpoint

--- homework-2/backend/app/main.py
+++ homework-2/backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import external_games, games, reviews, analytics, sessions
+from app.routers import external_news, external_games, games, reviews, analytics, sessions
 
 app = FastAPI()
 
@@ -12,3 +12,4 @@ app.include_router(reviews.router, prefix="/api")
 app.include_router(analytics.router, prefix="/api")
 app.include_router(sessions.router, prefix="/api")
 app.include_router(external_games.router, prefix="/api")
+app.include_router(external_news.router, prefix="/api")
---
+++ homework-2/backend/app/services/news_service.py
@@ -0,0 +1,40 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+NEWS_BASE_URL = "https://newsapi.org/v2"
+
+
+def get_gaming_news():
+    url = f"{NEWS_BASE_URL}/everything"
+
+    params = {
+        "q": '"video game" OR videogame OR gaming OR Steam OR PlayStation OR Xbox OR Nintendo',
+        "searchIn": "title,description",
+        "domains": "ign.com,gamespot.com,polygon.com,pcgamer.com,eurogamer.net,videogameschronicle.com",
+        "sortBy": "publishedAt",
+        "language": "en",
+        "pageSize": 10,
+        "apiKey": NEWS_API_KEY
+    }
+
+    response = requests.get(url, params=params, timeout=10)
+    response.raise_for_status()
+
+    data = response.json()
+
+    news = []
+
+    for article in data.get("articles", []):
+        news.append({
+            "title": article["title"],
+            "source": article["source"]["name"],
+            "url": article["url"],
+            "image": article["urlToImage"],
+            "published_at": article["publishedAt"]
+        })
+
+    return news
